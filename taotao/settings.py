e31b674d38ce62c0ee30bf3dda4462060631974e
fanlin <fanlin@qfpay.com>
2018-07-29 21:13:18 +0800

    delete error setting

--- taotao/settings.py
+++ taotao/settings.py
@@ -35,10 +35,10 @@ except ImportError:
     # celery的配置
     ###################################
 
-    RABBITMQ_USERNAME = 'iceihehe'
-    RABBITMQ_PASSWORD = 'iceihehe'
+    RABBITMQ_USERNAME = ''
+    RABBITMQ_PASSWORD = ''
     RABBITMQ_HOST = '192.168.2.55'
-    RABBITMQ_NAME = 'datatao'
+    RABBITMQ_NAME = ''
     SMS_QUEUE = 'sms_queue'
     EMAIL_QUEUE = 'email_queue'
 
@@ -69,23 +69,23 @@ except ImportError:
     DATA_DETAIL = '/search/'
 
     CALLBACK_URL = '/'
-    WEIBO_KEY = '840063179'
-    WEIBO_SECRET = 'b7e3959924bbf465460591f0660caae2'
+    WEIBO_KEY = ''
+    WEIBO_SECRET = ''
 
-    QQ_ID = '101258713'
-    QQ_SECRET = '77c6f65fcb8c3a00881acf9f20c6fa49'
+    QQ_ID = ''
+    QQ_SECRET = ''
 
     ###################################
     # 容联云通讯的配置
     ###################################
 
     # 注册模板
-    RLYUN_REG_TEMPID = 49033
+    RLYUN_REG_TEMPID = 4
     # 找回密码模板
-    RLYUN_FOR_TEMPID = 50263
-    RLYUN_SID = '8a48b5514f2b46d0014f2ffc9aca0596'
-    RLYUN_TOKEN = '0cff9cf8883f4c44a908466b92725f58'
-    RLYUN_APPID = '8a48b55150b36d920150b80c713210c7'
+    RLYUN_FOR_TEMPID = 5
+    RLYUN_SID = ''
+    RLYUN_TOKEN = ''
+    RLYUN_APPID = ''
 
     ###################################
     # 邮件的配置
@@ -96,12 +96,12 @@ except ImportError:
     # 找回密码用
     EMAIL_FOR = 1
     # KEY
-    EMAIL_API = 'key-c97c099861175cacc0b6b791cbd474dc'
-    EMAIL_FROM = u'数据淘 <datatao@wolongdata.com>'
+    EMAIL_API = ''
+    EMAIL_FROM = u''
     EMAIL_REG_TEXT = u'您的数据淘注册验证码是{0}，请在{1}分钟内输入验证码进行下一步操作。如非本人操作，请忽略此邮件。'
     EMAIL_FOR_TEXT = u'您的数据淘找回密码验证码是{0}，请在{1}分钟内输入验证码进行下一步操作。如非本人操作，请忽略此邮件。'
-    EMAIL_REG_SUBJECT = u'欢迎注册数据淘'
-    EMAIL_FOR_SUBJECT = u'找回密码-数据淘'
+    EMAIL_REG_SUBJECT = u''
+    EMAIL_FOR_SUBJECT = u''
     MAILGUN_SMTP_LOGIN = ("postmaster@sandbox012e3d186f2b4e8ab2536cacb9b3f713."
                           "mailgun.org")
     MAILGUN_SMTP_PASSWORD = "5401b1dd70c1939500bd9386d6dc1179"
@@ -113,11 +113,11 @@ except ImportError:
     # 七牛的配置
     ###################################
 
-    QINIU_ACCESS_KEY = 'sNb9mHAGhu_vToZl2YAj83rKbj0u682beqLgkxJt'
-    QINIU_SECRET_KEY = 'meZEkplL2f-6LnVmSfw21cAGNuHKbclVM1JAJlj4'
-    QINIU_BUCKET_NAME = 'datatao'
-    QINIU_BUCKET_NAME_PRI = 'datatao-data'
-    QINIU_IMAGE_URL = 'http://7xo2al.com1.z0.glb.clouddn.com/'
+    QINIU_ACCESS_KEY = ''
+    QINIU_SECRET_KEY = ''
+    QINIU_BUCKET_NAME = ''
+    QINIU_BUCKET_NAME_PRI = ''
+    QINIU_IMAGE_URL = ''
 
     ###################################
     # 日志的配置
@@ -129,14 +129,14 @@ except ImportError:
     # redis的配置
     ###################################
 
-    REDIS_HOST = '192.168.2.55'
+    REDIS_HOST = ''
     REDIS_TTL = 60
 
     ###################################
     # elasticsearch的配置
     ###################################
-    ES_HOST = '192.168.2.55'
-    ES_INDEX = 'datatao3'
-    ES_DOCTYPE = 'data'
-    ES_ANALYZER = 'chinese'
+    ES_HOST = ''
+    ES_INDEX = ''
+    ES_DOCTYPE = ''
+    ES_ANALYZER = ''
     # ES_ANALYZER = 'smartcn'
